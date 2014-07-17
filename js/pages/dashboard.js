53fdddaa78ebf5d70b06c5b62125d8a6be6ad876
karthi <karthikeyanar@gmail.com>
2014-07-17 09:48:41 +0530

    c3

--- js/pages/dashboard.js
+++ js/pages/dashboard.js
@@ -1,5 +1,15 @@
 ﻿$(function() {
-
+    var chart = c3.generate({
+        data: {
+            columns: [
+              ['data1', 30, 200, 100, 400, 150, 250],
+              ['data2', 50, 20, 10, 40, 15, 25]
+            ],
+            onclick: function (d, element) { console.log("onclick", d, element); },
+            onmouseover: function (d) { console.log("onmouseover", d); },
+            onmouseout: function (d) { console.log("onmouseout", d); },
+        }
+    });
 
 
 	var areaChart=Morris.Area({
