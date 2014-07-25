aca77aec0af48ee3bd60d84d6b859976190a6441
karthi <karthikeyanar@gmail.com>
2014-07-26 01:42:30 +0530

    sidebar right

--- js/files/Copy of application.js
+++ js/files/Copy of application.js
@@ -1,23 +1,28 @@
-﻿<div class="col-md-4">
-						<h3 class="no-margin-top">
-							Disabled
-						</h3>
-						<p>
-							.radio-circle for roundness.
-						</p>
-						<div class="radio metro-radio">
-							<label>
-								<input type="radio" disabled="" id="checkbox13" /><span></span>Can't check this
-							</label>
-						</div>
-						<div class="radio metro-radio metro-radio-success">
-							<label>
-								<input type="radio" disabled="" checked /><span></span>This too
-							</label>
-						</div>
-						<div class="radio metro-radio metro-radio-circle metro-radio-success">
-							<label>
-								<input type="radio" disabled="" checked /><span></span>And this
-							</label>
-						</div>
-					</div>
+﻿<div class="row">
+                        <div class="col-md-4">
+                            <h3 class="no-margin-top">
+                                Radio Inline
+                            </h3>
+                            <p>
+                                Supports bootstrap brand colors: .radio-primary, .radio-info etc.
+                            </p>
+                            <label class="radio-inline metro-radio">
+                                <input type="radio" />Default
+                            </label>
+                            <label class="radio-inline metro-radio metro-radio-primary">
+                                <input type="radio" checked />Primary
+                            </label>
+                            <label class="radio-inline metro-radio metro-radio-success">
+                                <input type="radio" checked />Success
+                            </label>
+                            <label class="radio-inline metro-radio metro-radio-info">
+                                <input type="radio" checked />Info
+                            </label>
+                            <label class="radio-inline metro-radio metro-radio-warning">
+                                <input type="radio" checked />Warning
+                            </label>
+                            <label class="radio-inline metro-radio metro-radio-danger">
+                                <input type="radio" checked />Danger
+                            </label>
+                        </div>
+                    </div>
